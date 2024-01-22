e25e1615e11f6fb92b78e47393f125c275af92c7
christian97dd <exequieldediego@gmail.com>
2024-01-21 21:44:35 -0300

    added recipe saving

--- app/meals/[mealsSlug]/page.js
+++ app/meals/[mealsSlug]/page.js
@@ -1,24 +1,36 @@
 import Image from 'next/image';
 import classes from './page.module.css';
+import { getMeal } from '@/lib/meals';
+import { notFound } from 'next/navigation';
 
-const MealsSlugPage = () => {
+const MealsSlugPage = ({params}) => {
+    const meal = getMeal(params.mealsSlug);
+
+    if (!meal) notFound();
+
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />')
+
     return (
         <>
             <header className={classes.header}>
                 <div className={classes.image}>
-                    <Image fill />
+                    <Image src={meal.image} alt={meal.title} fill />
                 </div>
                 <div className={classes.headerText}>
-                    <h1>TITLE</h1>
+                    <h1>{meal.title}</h1>
                     <p className={classes.creator}>
-                        by <a href={`mailto:${'email'}`}>NAME</a>
+                        by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
                     </p>
                     <p className={classes.summary}>
-                        SUMMARY
+                        {meal.summary}
                     </p>
                 </div>
             </header>
-            <main></main>
+            <main>
+                <p className={classes.instructions} dangerouslySetInnerHTML={{
+                    __html: meal.instructions
+                }}></p>
+            </main>
         </>
     )
 }
---
+++ components/Meals/image-picker.js
@@ -0,0 +1,45 @@
+'use client'
+
+import { useRef, useState } from 'react'
+import classes from './image-picker.module.css'
+import Image from 'next/image';
+
+const ImagePicker = ({label, name}) => {
+    const [pickedImage, setPickedImage] = useState('');
+    const imageInputRef = useRef(null)
+
+    const handlePickClick = () => imageInputRef.current.click();
+
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
+        if (!file) return setPickedImage('');
+
+        const fileReader = new FileReader();
+        fileReader.onload = () => setPickedImage(fileReader.result);
+        fileReader.readAsDataURL(file);
+    }
+
+    return (
+        <div className={classes.picker}>
+            <label htmlFor={name}>{label}</label>
+            <div className={classes.controls}>
+                <div className={classes.preview}>
+                    {!pickedImage && <p>No image picked yet</p>}
+                    {pickedImage && <Image src={pickedImage} alt='Image selected by user' fill />}
+                </div>
+                <input
+                    className={classes.input}
+                    type='file'
+                    id={name}
+                    accept='image/png, image/jpeg' 
+                    name={name} 
+                    ref={imageInputRef}
+                    onChange={handleImageChange}
+                />
+                <button className={classes.button} type='button' onClick={handlePickClick}>Pick an Image</button>
+            </div>
+        </div>
+    )
+}
+
+export default ImagePicker;
